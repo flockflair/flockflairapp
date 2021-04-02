f68c72a6552215d5d1fdc571e5aa6a1bda0ef177
sanikadeshmukh <69890327+sanikadeshmukh@users.noreply.github.com>
2021-04-02 20:40:28 +0530

    commit

--- app/src/main/java/com/example/flockflairapp/SplashScreen.java
+++ app/src/main/java/com/example/flockflairapp/SplashScreen.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -28,6 +29,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         setContentView(R.layout.activity_splash_screen);
         //Animations
 
@@ -43,9 +45,7 @@ public class SplashScreen extends AppCompatActivity {
         name = findViewById(R.id.appname);
         logo = findViewById(R.id.logo);
 
-        testube.setRotation(345);
-        //physics.setRotation(5);
-        bacteria.setRotation(320);
+
 
 
 
@@ -70,5 +70,17 @@ public class SplashScreen extends AppCompatActivity {
 
 
 
+    }
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
     }
 }
