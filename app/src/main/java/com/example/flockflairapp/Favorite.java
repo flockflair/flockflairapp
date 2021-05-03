950c855f32f257403f090c4839d254cec37e8e1b
RoNe <devang458@gmail.com>
2021-05-03 16:16:10 +0530

    Favorite feature

--- app/src/main/java/com/example/flockflairapp/Favorite.java
+++ app/src/main/java/com/example/flockflairapp/Favorite.java
@@ -1,5 +1,7 @@
 package com.example.flockflairapp;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -7,38 +9,51 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Favorite {
     private FirebaseDatabase ds = FirebaseDatabase.getInstance();
-    private DatabaseReference d = ds.getReference();
+    private DatabaseReference favRef = ds.getReference();
     private String uid = FirebaseAuth.getInstance().getUid();
 
-    public void setFavorite(){
-        FavoriteModel favoriteModel = new FavoriteModel();
-        d.child("user").child(uid).child("Favorite").push().setValue(favoriteModel);
+    public void setFavorite(FavoriteModel favoriteModel){
+        favRef.child("user").child(uid).child("Favorite").push().setValue(favoriteModel);
     }
 
-    public void getFavorite(final String pushKey){
-        FavoriteModel model = new FavoriteModel();
-        List<String> FavkeyList = new ArrayList<>();
+    public void getFavorite(List<FavoriteModel> getFavoriteName){
 
-        d.child("user").child(uid).child("Favorite").addListenerForSingleValueEvent(new ValueEventListener() {
+        favRef.child("user").child(uid).child("Favorite").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String key = pushKey;
                 for (DataSnapshot ds:snapshot.getChildren()){
-                    key = ds.getKey();
-                    FavkeyList.add(key);
+                    getFavoriteName.add(ds.getValue(FavoriteModel.class));
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("getFavoriteName", error.getMessage());
+            }
+        });
+    }
 
+    public void removeFavorite(){
+        FavoriteModel model = new FavoriteModel();
+
+        Query removeFavorite = favRef.child("user").child(uid).child("Favorite").orderByChild("FavoriteName").equalTo(model.getFavoriteName());
+
+        removeFavorite.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot:snapshot.getChildren()){
+                    dataSnapshot.getRef().removeValue();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("removeFavorite", error.getMessage());
             }
         });
     }
