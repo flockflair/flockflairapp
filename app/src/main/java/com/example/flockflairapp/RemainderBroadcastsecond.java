4f851eb7647b1909009b260b470145f2847ea443
Rahul Pudurkar <rahulpudurkar68@gmail.com>
2021-06-16 01:06:37 +0530

    commit

---
+++ app/src/main/java/com/example/flockflairapp/RemainderBroadcastsecond.java
@@ -0,0 +1,32 @@
+package com.example.flockflairapp;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class RemainderBroadcastsecond extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        intent = new Intent(context, QuestionOfTheDay.class);
+        // intent.putExtra("STD11",11);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "TheBotanist")
+                .setSmallIcon(R.drawable.thebotanist)
+                .setContentTitle("Question of the day")
+                .setContentText("second notification")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(1, builder.build());
+
+    }
+}
