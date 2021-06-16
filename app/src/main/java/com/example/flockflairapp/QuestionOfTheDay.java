e23b095b3bc0bc16c71c5177327d0b556f165393
RoNe <devang458@gmail.com>
2021-06-16 23:59:52 +0530

    update

--- app/src/main/java/com/example/flockflairapp/QuestionOfTheDay.java
+++ app/src/main/java/com/example/flockflairapp/QuestionOfTheDay.java
@@ -1,7 +1,5 @@
 package com.example.flockflairapp;
 
-import android.app.ActivityManager;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
